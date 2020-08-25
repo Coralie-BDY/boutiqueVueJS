183861a11f7d466b44e50f69d6de15cf4daf6fd2
Coralie Baudry <coralie.bdy@gmail.com>
2020-08-25 09:59:16 +0200

    first commit

---
+++ src/components/TheHeader.vue
@@ -0,0 +1,68 @@
+<template>
+  <transition appear>  
+    <nav class="navbar navbar-expand-lg navbar-light bg-light">
+      <a class="navbar-brand" href="#">
+        <img src="../assets/logo.png" alt="Vuelogo" width="30" height="30">
+      Ma Boutique
+      </a>
+      <button class="navbar-toggler">
+        <span class="navbar-toggler-icon" v-trigger-collapse="'collapse'"></span>
+      </button>
+      <div id="collapse" class="collapse navbar-collapse">
+        <ul class="navbar-nav">
+          <li class="nav-item">
+            <router-link class="nav-link" to="/shop">Boutique</router-link>
+          </li>
+          <li class="nav-item">
+            <router-link class="nav-link" to="/admin">Admin</router-link>
+          </li>
+        </ul>
+      </div>
+    </nav>
+  </transition>
+</template>
+
+<script>
+export default {
+  directives: {
+    triggerCollapse: {
+      inserted(el, binding) {
+        const nav = document.querySelector(`#${ binding.value }`);
+
+        window.addEventListener('click', () => {
+          nav.classList.remove('show');
+        });
+
+        
+        el.addEventListener('click', (e) => {
+          e.stopPropagation();
+
+          if (nav.classList.contains('show')) {
+            nav.classList.remove('show');
+          } else {
+            nav.classList.add('show');
+          }
+        });
+      }
+    }
+  }
+}
+</script>
+
+<style scoped>
+  @keyframes fromtop {
+    from {
+      opacity: .3;
+      transform: translateY(-20px);
+    }
+    to {}
+  }
+
+  .v-enter-active {
+    animation: fromtop 1s ease;
+  }
+
+  .router-link-active {
+    font-weight: bold;
+  }
+</style>
---
+++ src/components/features/User/User.vue
@@ -0,0 +1,50 @@
+<template>
+  <div>
+    <div class="d-flex flex-row">
+      <transition appear name="left">
+        <shop class="w-75"></shop>
+      </transition>
+      <transition appear name="right">
+        <cart class="w-25"></cart>
+      </transition>
+    </div>
+  </div>
+</template>
+
+<script>
+import Shop from './Shop/Shop';
+import Cart from './Cart/Cart';
+
+export default {
+  components: {
+    Shop,
+    Cart
+  }
+}
+</script>
+
+<style scoped>
+  @keyframes fromleft {
+    from {
+      opacity: .3;
+      transform: translateX(-20px);
+    }
+    to {}
+  }
+
+  @keyframes fromright {
+    from {
+      opacity: .3;
+      transform: translateX(20px);
+    }
+    to {}
+  }
+
+  .left-enter-active {
+    animation: fromleft 1s;
+  }
+
+  .right-enter-active {
+    animation: fromright 1s;
+  }
+</style>
